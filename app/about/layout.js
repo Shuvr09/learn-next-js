0f436145f7bdb7c9c2b571e20146fd9a3ba67879
Shuvr09 <shuvrodas0907@gmail.com>
2025-06-12 18:48:27 +0600

    learned routing

---
+++ app/about/layout.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+
+export default function AboutLayout({ children }) {
+  return (
+    <div>
+      <nav className="mbr-6 mb-6">
+        <ul className="flex gap-6">
+          <li>
+            <Link href="/about/vision">Vision</Link>
+          </li>
+          <li>
+            <Link href="/about/mission">Mission</Link>
+          </li>
+        </ul>
+        {children}
+      </nav>
+    </div>
+  );
+}
---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <main className="mt-10">
+      <div>About page content goes here.</div>
+    </main>
+  );
+}
