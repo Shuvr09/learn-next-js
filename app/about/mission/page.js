76689269f183876650f1e4ec749a4156ef315896
Shuvr09 <shuvrodas0907@gmail.com>
2025-06-12 20:56:47 +0600

    image optimization

--- app/about/mission/page.js
+++ app/about/mission/page.js
@@ -1,10 +1,22 @@
 import Button from "@/app/components/button";
+import Image from "next/image";
 import React from "react";
+import myimage from "@/public/images/Shuvro_Ghibli.png";
 
 export default function Mission() {
   return (
     <main className="mt-10">
       <div>This is mission page</div>
+
+      <div className="w-[300px]">
+        <Image
+          placeholder="blur"
+          src={myimage}
+          alt="My Image in Ghibli"
+          quality={100}
+        />
+      </div>
+
       <Button />
     </main>
   );
