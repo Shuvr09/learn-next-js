8372a9586e5fa75c442b647cb8935f0b6137c62e
Shuvr09 <shuvrodas0907@gmail.com>
2025-06-13 13:39:18 +0600

    dynamic meta data & progressive rendering

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,13 +1,36 @@
 import getPost from "@/lib/getPost";
+import getPostComments from "@/lib/getPostComment";
 import React from "react";
+
+export async function generateMetadata({ params }) {
+  const { id } = await params;
+  const post = await getPost(id);
+  return {
+    title: post.title,
+    description: post.body,
+  };
+}
 
 export default async function PostPage({ params }) {
   const { id } = await params;
-  const post = await getPost(id);
+  const postPromise = await getPost(id);
+  const commentPromise = await getPostComments(id);
+
+  const [post, comments] = await Promise.all([postPromise, commentPromise]);
+
   return (
     <div className="mt-6">
       <h2 className="text-blue-500">{post.title}</h2>
       <p className="mt-3">{post.body}</p>
+      <hr></hr>
+      <div className="mt-10">
+        <h1>Comments</h1>
+        <ul>
+          {comments.map((comment) => (
+            <li key={comment.id}>{comment.body}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
